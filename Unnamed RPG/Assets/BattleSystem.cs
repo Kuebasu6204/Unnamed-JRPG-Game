24374a4e821085aea763a10e6b37d2405d2fe594
Adrian <36515623+manzanoa@users.noreply.github.com>
2022-04-03 15:11:19 -0400

    Why

---
+++ Unnamed RPG/Assets/BattleSystem.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYER, ENEMY, WIN, LOSE}
+
+
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+
+    public Text dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    
+
+    public BattleState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetUpBattle());
+        
+    }
+
+    IEnumerator SetUpBattle()
+    {
+        dialogueText.text = "A wild " + enemy.GetComponent<Unit>().unitName + " appears!";
+        playerHUD.setHUD(player.GetComponent<Unit>());
+        enemyHUD.setHUD(enemy.GetComponent<Unit>());
+
+        yield return new WaitForSeconds(3f);
+
+        state = BattleState.PLAYER;
+
+        PlayerTurn();
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose!";
+    }
+
+    public void OnAttack()
+    {
+        if (state != BattleState.PLAYER)
+            return;
+
+        StartCoroutine(Attack());
+    }
+
+    IEnumerator Attack()
+    {
+        bool isDead = enemy.GetComponent<Unit>().takeDamage(player.GetComponent<Unit>().attack);
+        enemyHUD.setHUD(enemy.GetComponent<Unit>());
+
+        dialogueText.text = enemy.GetComponent<Unit>().unitName + " recieved damage!";
+
+        yield return new WaitForSeconds(2);
+
+        if(isDead)
+        {
+            state = BattleState.WIN;
+            dialogueText.text = "YOU WIN!!!";
+            //victory
+        }
+        else
+        {
+            state = BattleState.ENEMY;
+            //continue to enemy turn
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemy.GetComponent<Unit>().unitName + " is attacking!!";
+
+        yield return new WaitForSeconds(1);
+
+        bool isDead = player.GetComponent<Unit>().takeDamage(enemy.GetComponent<Unit>().attack);
+        playerHUD.setHUD(player.GetComponent<Unit>());
+
+        yield return new WaitForSeconds(1);
+
+        if (isDead)
+        {
+            state = BattleState.LOSE;
+            dialogueText.text = "YOU LOSE!!";
+        }
+        else
+        {
+            state = BattleState.PLAYER;
+            PlayerTurn();
+        }
+    }
+
+}
---
+++ Unnamed RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    
+
+
+    
+
+    public float timer = 0;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if(timer > 2 && timer < 4)
+            {
+                //attack * 2
+            }
+            else if(timer < 0)
+            {
+                //attack / 2
+            }
+            else
+            {
+                //attack
+            }
+        }
+    }
+
+    void Attack()
+    {
+        
+    }
+}
